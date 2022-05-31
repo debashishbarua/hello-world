12c31d688817bc739dd94cf519708a224a6dd4c7
Debashish Barua <debashish_aec@yahoo.co.in>
2022-05-31 11:37:28 +0530

    New End Point Say hello to XX

--- hello-world/src/main/java/com/cognizant/controller/HelloController.java
+++ hello-world/src/main/java/com/cognizant/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.cognizant.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class HelloController {
 	@GetMapping("/hello")
 	public String sayHello() {
 		return "Hello World!!!!";
+	}
+	
+	@GetMapping("/hello/{name}")
+	public String sayHelloToXX(@PathVariable("name") String name) {
+		return "Hello Mr. " +name;
 	}
 }
